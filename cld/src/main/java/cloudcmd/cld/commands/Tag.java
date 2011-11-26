4009a266c70dc45c4b4ca9278872a4bf9c5533fb
Brian Guarraci <brian@ops5.com>
2011-11-26 00:14:22 -0800

    add sqlite storage

--- cld/src/main/java/cloudcmd/cld/commands/Tag.java
+++ cld/src/main/java/cloudcmd/cld/commands/Tag.java
@@ -1,14 +1,17 @@
 package cloudcmd.cld.commands;
 
 
-import jpbetz.cli.Command;
-import jpbetz.cli.CommandContext;
-import jpbetz.cli.CommandError;
-import jpbetz.cli.SubCommand;
+import jpbetz.cli.*;
 
 @SubCommand(name="reindex", description="Rebuild the index from the archived files.")
 public class Tag implements Command {
-	
+
+  @Arg(name="tags", optional = true, isVararg = true)
+  public String[] _tags = null;
+
+  @Opt(opt = "r", longOpt = "remove", description = "remove tags", required = false)
+  public boolean _remove;
+
 	@Override
   public void exec(CommandContext commandLine) throws CommandError, Exception {
   }
