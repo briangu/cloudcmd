7e1bd50c29abcc4ae42f498bd2eaa7f2ec25a5ea
Brian Guarraci <brian@ops5.com>
2012-06-23 18:52:24 -0700

    make fileadapter use h2 as a cache

--- common/src/main/java/cloudcmd/common/adapters/FileAdapter.java
+++ common/src/main/java/cloudcmd/common/adapters/FileAdapter.java
@@ -1,16 +1,18 @@
 package cloudcmd.common.adapters;
 
-import cloudcmd.common.CryptoUtil;
-import cloudcmd.common.FileHandler;
-import cloudcmd.common.FileUtil;
-import cloudcmd.common.FileWalker;
+import cloudcmd.common.*;
+import org.h2.jdbcx.JdbcConnectionPool;
+import org.jets3t.service.model.S3Object;
 import org.json.JSONException;
 
 import java.io.*;
 import java.net.URI;
+import java.sql.*;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 //     "file:///tmp/storage?tier=1&tags=image,movie,vacation"
 
@@ -19,6 +21,8 @@ public class FileAdapter extends Adapter
   private final static int MIN_FREE_STORAGE_SIZE = 1024 * 1024;
 
   String _rootPath;
+  JdbcConnectionPool _cp = null;
+  Set<String> _description = null;
 
   public FileAdapter()
   {
@@ -31,12 +35,95 @@ public class FileAdapter extends Adapter
 
     _rootPath = URI.getPath();
 
-    initSubDirs(_rootPath);
-
     File rootPathDir = new File(_rootPath);
     rootPathDir.mkdirs();
 
     _isOnline = rootPathDir.exists();
+
+    if (_isOnline) bootstrap(_rootPath);
+  }
+
+  private String getDbFile()
+  {
+    return String.format("%s%sindex", ConfigDir, File.separator);
+  }
+
+  private String createConnectionString()
+  {
+    return String.format("jdbc:h2:%s", getDbFile());
+  }
+
+  private Connection getDbConnection() throws SQLException
+  {
+    return _cp.getConnection();
+  }
+
+  private Connection getReadOnlyDbConnection() throws SQLException
+  {
+    Connection conn = getDbConnection();
+    conn.setReadOnly(true);
+    return conn;
+  }
+
+  private void bootstrap(String rootPath)
+    throws Exception
+  {
+    Class.forName("org.h2.Driver");
+    _cp = JdbcConnectionPool.create(createConnectionString(), "sa", "sa");
+    File file = new File(getDbFile() + ".h2.db");
+    if (!file.exists())
+    {
+      bootstrapDb();
+      initSubDirs(rootPath);
+    }
+  }
+
+  private void bootstrapDb()
+  {
+    Connection db = null;
+    Statement st = null;
+    try
+    {
+      db = getDbConnection();
+      st = db.createStatement();
+
+      st.execute("DROP TABLE if exists BLOCK_INDEX;");
+      st.execute("CREATE TABLE BLOCK_INDEX ( HASH VARCHAR PRIMARY KEY );");
+
+      db.commit();
+    }
+    catch (SQLException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      SqlUtil.SafeClose(st);
+      SqlUtil.SafeClose(db);
+    }
+  }
+
+  public void purge()
+  {
+    Connection db = null;
+    Statement st = null;
+    try
+    {
+      db = getDbConnection();
+      st = db.createStatement();
+      st.execute("delete from BLOCK_INDEX;");
+
+      _description = null;
+    }
+    catch (SQLException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      SqlUtil.SafeClose(st);
+      SqlUtil.SafeClose(db);
+    }
   }
 
   @Override
@@ -63,17 +150,51 @@ public class FileAdapter extends Adapter
   @Override
   public void refreshCache() throws Exception
   {
+    purge();
+
+    Connection db = null;
+    PreparedStatement statement = null;
+    try
+    {
+      db = getDbConnection();
+
+      db.setAutoCommit(false);
+
+      statement = db.prepareStatement("INSERT INTO BLOCK_INDEX VALUES (?)");
+
+      for (String hash : rebuildHashIndexFromDisk())
+      {
+        statement.setString(1, hash);
+        statement.execute();
+        statement.clearParameters();
+      }
+
+      db.commit();
+    }
+    catch (SQLException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      SqlUtil.SafeClose(statement);
+      SqlUtil.SafeClose(db);
+    }
   }
 
   @Override
   public boolean contains(String hash) throws Exception
   {
-    return new File(getDataFileFromHash(hash)).exists();
+    return _description.contains(hash);
   }
 
   @Override
   public void shutdown()
   {
+    if (_cp != null)
+    {
+      _cp.dispose();
+    }
   }
 
   @Override
@@ -111,6 +232,7 @@ public class FileAdapter extends Adapter
   public void store(InputStream is, String hash) throws Exception
   {
     FileUtil.writeFile(is, getDataFileFromHash(hash));
+    insertHash(hash);
   }
 
   @Override
@@ -133,6 +255,7 @@ public class FileAdapter extends Adapter
         throw new IOException("failed to move file: " + tmpFile.getAbsolutePath());
       }
     }
+    insertHash(hash);
     return hash;
   }
 
@@ -144,8 +267,82 @@ public class FileAdapter extends Adapter
     return new FileInputStream(file);
   }
 
+  private void insertHash(String hash) throws Exception
+  {
+    Connection db = null;
+    PreparedStatement statement = null;
+    try
+    {
+      db = getDbConnection();
+
+      db.setAutoCommit(false);
+
+      statement = db.prepareStatement("INSERT INTO BLOCK_INDEX VALUES (?)");
+      statement.setString(1, hash);
+      statement.execute();
+
+      db.commit();
+
+      _description.add(hash);
+    }
+    catch (SQLException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      SqlUtil.SafeClose(statement);
+      SqlUtil.SafeClose(db);
+    }
+  }
+
   @Override
   public Set<String> describe()
+    throws Exception
+  {
+    if (_description == null)
+    {
+      _description = _describe();
+    }
+
+    return _description;
+  }
+
+  private Set<String> _describe()
+    throws Exception
+  {
+    Set<String> description = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
+
+    Connection db = null;
+    PreparedStatement statement = null;
+
+    try
+    {
+      db = getReadOnlyDbConnection();
+
+      statement = db.prepareStatement("SELECT * FROM BLOCK_INDEX");
+
+      ResultSet resultSet = statement.executeQuery();
+
+      while (resultSet.next())
+      {
+        description.add(resultSet.getString("HASH"));
+      }
+    }
+    catch (SQLException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      SqlUtil.SafeClose(statement);
+      SqlUtil.SafeClose(db);
+    }
+
+    return Collections.unmodifiableSet(description);
+  }
+
+  public Set<String> rebuildHashIndexFromDisk()
   {
     final Set<String> hashes = new HashSet<String>();
 
