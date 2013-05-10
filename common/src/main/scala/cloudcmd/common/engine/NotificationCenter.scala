1247df6673351a02c41c88e257cdc77fcb1488f5
Brian Guarraci <bguarraci@twitter.com>
2013-05-10 16:45:39 -0700

    wip

--- common/src/main/scala/cloudcmd/common/engine/NotificationCenter.scala
+++ common/src/main/scala/cloudcmd/common/engine/NotificationCenter.scala
@@ -1,6 +1,7 @@
 package cloudcmd.common.engine
 
 import scala.collection.mutable
+import java.util.concurrent.SynchronousQueue
 
 object NotificationCenter {
 
@@ -14,60 +15,97 @@ object NotificationCenter {
 
 class NotificationCenter {
 
-  class Subscription(name: String, source: Any) {
+  case class Message(name: String, source: Option[Any], userInfo: Option[Map[String, Any]])
 
-    def removeObserverForSource(observer: Any, source: Any) {
+  val event: Array[Boolean] = new Array[Boolean](1)
+  event(0) = false
 
+  val queue = new SynchronousQueue[Message]
+
+  val msgPump: Thread = new Thread(new Runnable {
+    def run() {
+      while (!event(0)) {
+        try {
+          val msg = queue.take
+          _center.get(msg.name) match {
+            case Some(subscription) => {
+              subscription.postNotification(msg.source, msg.userInfo)
+            }
+          }
+          System.err.println(msg)
+        }
+        catch {
+          case e: InterruptedException => ;
+        }
+      }
+    }
+  })
+
+  class Subscriptions(name: String) {
+
+    case class Subscription(observer: Any, source: Any, fn: Option[Map[String, Any]] => Unit)
+
+    var observers = new mutable.HashSet[Subscription] with mutable.SynchronizedSet[Subscription]
+
+    def removeObserver(observer: Any, source: Option[Any]) {
+      val toRemove = source match {
+        case Some(_) => {
+          observers filter { s => s.observer == observer && s.source == source }
+        }
+        case None => {
+          observers filter { _.observer == observer }
+        }
+      }
+      toRemove.foreach(observers.remove)
     }
 
-    def removeObserver(observer: Any) {
-
+    def addObserver(observer: Any, source: Option[Any], fn: Option[Map[String, Any]] => Unit) {
+      observers.add(new Subscription(observer, source, fn))
     }
 
-    def addObserverForSource(observer: Any, source: Any) {
-
-    }
-
-    def addObserver(observer: Any) {
-
+    def postNotification(source: Option[Any] = None, userInfo: Option[Map[String, Any]] = None) {
+      source match {
+        case Some(_) => {
+          observers filter { _.source == source } foreach { observer => observer.fn(userInfo) }
+        }
+        case None => {
+          observers foreach { observer => observer.fn(userInfo) }
+        }
+      }
     }
 
   }
 
-  // name -> (name, obj) -> subscribers
+  val _center = new mutable.HashMap[String, Subscriptions] with mutable.SynchronizedMap[String, Subscriptions]
 
-  val _center = new mutable.HashMap[String, Subscription] with mutable.SynchronizedMap[String, Subscription]
-
-  def removeObserverForName(observer: Any, name: String, source: Any) {
+  def removeObserverForName(observer: Any, name: String, source: Option[Any]) {
     _center.get(name) match {
       case Some(subscription) => {
-        subscription.removeObserverForSource(observer, source)
+        subscription.removeObserver(observer, source)
       }
       case None => ;
     }
   }
 
   def removeObserver(observer: Any) {
-    _center.values.foreach(_.removeObserver(observer))
+    _center.values.foreach(_.removeObserver(observer, None))
   }
 
-  def addObserverForName(observer: Any, name: String, source: Option[Any]) {
+  def addObserverForName(observer: Any, name: String, source: Option[Any], fn: Option[Map[String, Any]] => Unit) {
     _center.get(name) match {
       case Some(subscription) => {
-        source match {
-          case Some(_) => {
-            subscription.addObserverForSource(observer, source)
-          }
-          case None => {
-            subscription.addObserver(observer)
-          }
-        }
+        subscription.addObserver(observer, source, fn)
+      }
+      case None => {
+        val subscription = new Subscriptions(name)
+        _center.put(name, subscription)
+        subscription.addObserver(observer, source, fn)
       }
     }
   }
 
   def postNotification(name: String, source: Option[Any] = None, userInfo: Option[Map[String, Any]] = None) {
-
+    queue.add(new Message(name, source, userInfo))
   }
 
 }
