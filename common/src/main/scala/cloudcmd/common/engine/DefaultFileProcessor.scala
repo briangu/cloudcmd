175416fab2529bdda23c33370ab31db8ce78a432
Brian Guarraci <bguarraci@twitter.com>
2013-05-22 21:10:11 -0700

    wrap all use of fis in a try

--- common/src/main/scala/cloudcmd/common/engine/DefaultFileProcessor.scala
+++ common/src/main/scala/cloudcmd/common/engine/DefaultFileProcessor.scala
@@ -2,7 +2,7 @@ package cloudcmd.common.engine
 
 import java.io.{ByteArrayInputStream, File}
 import cloudcmd.common.{ContentAddressableStorage, RandomAccessFileInputStream, FileMetaData, FileUtil}
-import cloudcmd.common.util.{JsonUtil, FileTypeUtil, CryptoUtil}
+import cloudcmd.common.util.{JsonUtil, CryptoUtil}
 import org.json.JSONObject
 import org.apache.log4j.Logger
 
@@ -14,56 +14,59 @@ class DefaultFileProcessor(cas: ContentAddressableStorage) extends FileProcessor
     var blockHash: String = null
 
     val fis = RandomAccessFileInputStream.create(file)
-    try {
-      fis.mark(0)
-      blockHash = CryptoUtil.computeHashAsString(fis)
-    } finally {
-      fis.reset()
-    }
-
-    // Include the extension has this is most likely the tag that adapters will filter by.
-    val fileExtensionIdx = file.getName.lastIndexOf('.')
-    val extendedTags = if (fileExtensionIdx >= 0) {
-      val fileExtension = file.getName.substring(fileExtensionIdx + 1)
-      tags ++ Set(fileExtension)
-    } else {
-      tags
-    }
-
-    val rawFmd =
-      JsonUtil.createJsonObject(
-        "path", file.toURI.toASCIIString,
-        "size", file.length.asInstanceOf[AnyRef],
-        "date", file.lastModified.asInstanceOf[AnyRef],
-        "blocks", JsonUtil.toJsonArray(List(blockHash)),
-        "tags", JsonUtil.toJsonArray(extendedTags))
-
-    if (properties != null && properties.length() > 0) {
-      rawFmd.put("properties", properties)
-    }
-
-    val fmd = FileMetaData.create(rawFmd)
 
     try {
-      cas.store(fmd.createBlockContext(blockHash), fis)
-    } catch {
-      case e:Exception => {
-        log.error("failed to index block for %s".format(file.getAbsoluteFile), e)
-        throw e
+      try {
+        fis.mark(0)
+        blockHash = CryptoUtil.computeHashAsString(fis)
+      } finally {
+        fis.reset()
       }
+
+      // Include the extension has this is most likely the tag that adapters will filter by.
+      val fileExtensionIdx = file.getName.lastIndexOf('.')
+      val extendedTags = if (fileExtensionIdx >= 0) {
+        val fileExtension = file.getName.substring(fileExtensionIdx + 1)
+        tags ++ Set(fileExtension)
+      } else {
+        tags
+      }
+
+      val rawFmd =
+        JsonUtil.createJsonObject(
+          "path", file.toURI.toASCIIString,
+          "size", file.length.asInstanceOf[AnyRef],
+          "date", file.lastModified.asInstanceOf[AnyRef],
+          "blocks", JsonUtil.toJsonArray(List(blockHash)),
+          "tags", JsonUtil.toJsonArray(extendedTags))
+
+      if (properties != null && properties.length() > 0) {
+        rawFmd.put("properties", properties)
+      }
+
+      val fmd = FileMetaData.create(rawFmd)
+
+      try {
+        cas.store(fmd.createBlockContext(blockHash), fis)
+      } catch {
+        case e:Exception => {
+          log.error("failed to index block for %s".format(file.getAbsoluteFile), e)
+          throw e
+        }
+      }
+
+      try {
+        cas.store(fmd.createBlockContext, new ByteArrayInputStream(fmd.getDataAsString.getBytes("UTF-8")))
+      } catch {
+        case e:Exception => {
+          log.error("failed to index meta for %s".format(file.getAbsoluteFile), e)
+          throw e
+        }
+      }
+
+      fmd
     } finally {
       FileUtil.SafeClose(fis)
     }
-
-    try {
-      cas.store(fmd.createBlockContext, new ByteArrayInputStream(fmd.getDataAsString.getBytes("UTF-8")))
-    } catch {
-      case e:Exception => {
-        log.error("failed to index meta for %s".format(file.getAbsoluteFile), e)
-        throw e
-      }
-    }
-
-    fmd
   }
 }
